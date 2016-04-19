56282971c76536b4596bd7e1436746136146c8fb
andreyevdakov <andreyevdakov@gmail.com>
2016-04-19 23:23:24 +0300

    User only Sign code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var crypto = require('crypto');
 var fs = require('fs');
-/*
+
 var privatePem = fs.readFileSync('key.pem');
 var publicPem = fs.readFileSync('cert.pem');
 var key = privatePem.toString();
@@ -17,10 +17,4 @@ var verify = crypto.createVerify('RSA-SHA512');
 verify.update(data);
 var result = verify.verify(pubkey, sig, 'base64');
 console.log('verification: ', result);
-*/
 
-var pem = fs.readFileSync('slsrvmgr.ske');
-var key = pem.toString('ascii');
-var hmac = crypto.createHmac('sha512', key);
-hmac.update('foosaljf;asdj;asjd;alsjd;flsjdfsidpf89we8fpw98efpw89euf');
-console.log(hmac.digest('base64'));
